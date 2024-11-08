45910358642305a25883255cab635846aaf37fc4
lsalug00 <lidiasaludes@gmail.com>
2024-11-08 01:18:35 +0100

    utils

--- src/utils.js
+++ src/utils.js
@@ -1,24 +1,25 @@
-// src/utils.js
-
 /**
  * Verifica si un número es potencia de 2.
  * @param {number} numero 
  * @returns {boolean}
  */
 function esPotenciaDeDos(numero) {
-    return (numero & (numero - 1)) === 0 && numero !== 0;
-  }
-  
-  /**
-   * Mezcla un array de manera aleatoria (Fisher-Yates Shuffle).
-   * @param {Array} array 
-   */
-  function mezclarArray(array) {
+    return (numero > 0) && (numero & (numero - 1)) === 0;
+}
+
+/**
+ * Mezcla un array de manera aleatoria (Fisher-Yates Shuffle).
+ * @param {Array} array 
+ */
+function mezclarArray(array) {
+    // Aseguramos que el array no esté vacío para evitar errores
+    if (array.length <= 1) return;
+
     for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]]; // Intercambio de elementos
     }
-  }
+}
   
-  module.exports = { esPotenciaDeDos, mezclarArray };
+module.exports = { esPotenciaDeDos, mezclarArray };
   
